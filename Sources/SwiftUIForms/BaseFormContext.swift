d8eeaf7056c44d4af807294f8c179886ae79e008
kaiobrito <kaio.a7x@gmail.com>
2020-04-20 18:38:09 -0300

    Make BaseFormContext attributes public

--- Sources/SwiftUIForms/BaseFormContext.swift
+++ Sources/SwiftUIForms/BaseFormContext.swift
@@ -32,8 +32,8 @@ open class BaseFormContext<ValueType: FormData>: FormContext {
     public var initialValues: ValueType
     @Published public var value: ValueType
     @Published public var errors: ValidationErrors?
-    var onValidate: ValidationMethod?
-    var onSubmit: SubmissionActionType?
+    public var onValidate: ValidationMethod?
+    public var onSubmit: SubmissionActionType?
     private var validationCancellable: AnyCancellable!
 
     public init(_ value: ValueType,
